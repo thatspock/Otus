c7b4ba084884c38bf27e6b5ce8d29ca143b94c0f
thatspock <thatspock@gmail.com>
2022-06-14 20:16:05 +0300

    navigation

---
+++ Test-Navigation/Test-Navigation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test-Navigation
+//
+//  Created by mr spock on 6/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test-Navigation/Test-Navigation/Test_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_NavigationApp.swift
+//  Test-Navigation
+//
+//  Created by mr spock on 6/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Test_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
